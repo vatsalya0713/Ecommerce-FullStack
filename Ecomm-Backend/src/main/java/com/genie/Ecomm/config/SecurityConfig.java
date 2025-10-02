2bccc8ebac776ab84b42f2ba6529eba12d470056
Vatsalya Bhardwaj <shaktisingh7292@gmail.com>
2025-10-02 15:10:16 +0530

    updating url in SecurityConfig file

--- Ecomm-Backend/src/main/java/com/genie/Ecomm/config/SecurityConfig.java
+++ Ecomm-Backend/src/main/java/com/genie/Ecomm/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-                "https://ecommerce-fullstack-uq1h.onrender.com"
+                "https://ecommerce-fullstack-uq1h.onrender.com/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
